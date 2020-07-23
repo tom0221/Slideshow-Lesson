637c183092c77d8785672cb71dbec0bca7a4db74
Tatsuro Muto <tomuto0221@gmail.com>
2020-07-23 18:47:30 +0900

    [finish]rails5まで

---
+++ tweet_app/db/.backup.sql
@@ -0,0 +1,14 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20170331045923');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2017-03-31 05:21:00.324397','2017-03-31 05:21:00.324397');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'次は何の本を読もうかな。','2017-03-31 05:24:15.529063','2017-03-31 05:24:15.529063');
+INSERT INTO posts VALUES(2,'今日のランチおいしかった。','2017-03-31 05:24:32.004378','2017-03-31 05:24:32.004378');
+INSERT INTO posts VALUES(4,replace(replace('たまには復習もしようかな！どう思う？?\r\n','\r',char(13)),'\n',char(10)),'2020-07-22 02:01:15.144007','2020-07-22 02:26:54.147352');
+INSERT INTO posts VALUES(6,'もう直ぐお昼ですね！','2020-07-22 02:48:23.515738','2020-07-22 02:48:23.515738');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',6);
+COMMIT;
